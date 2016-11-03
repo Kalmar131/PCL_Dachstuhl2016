217dcedbceb00d65abb87f29e59e6a5cdf6c9b6e
Kalmar131 <github@orenn.de>
2016-11-03 17:37:32 +0100

    add project for merging and downsampling of clouds
    note: build/merged.pcd and build/merged_downsampled_0.05.pcd are omitted
    because they are too large for github. simply run the binary to create
    them.

---
+++ merge_and_downsample/build/CMakeFiles/merge_and_downsample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/merge_and_downsample.dir/merge_and_downsample.cpp.o"
+  "merge_and_downsample.pdb"
+  "merge_and_downsample"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/merge_and_downsample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
