35d88717dce0ce47d295ccfdd8df10931ac688a5
Kalmar131 <github@orenn.de>
2016-11-03 18:07:53 +0100

    add project for wall removal
    incomplete, removal of roof still missing

---
+++ remove_walls/build/CMakeFiles/remove_walls.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/remove_walls.dir/remove_walls.cpp.o"
+  "remove_walls.pdb"
+  "remove_walls"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/remove_walls.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
